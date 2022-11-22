8116f3e3ec61c6265adedcbb9a0eb020da72944b
KV <nmp1.testing@gmail.com>
2022-11-22 23:19:59 +1100

    add new class

---
+++ NewBranchFrMaster.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class NewBranchFrMaster
+    {
+        private string BrFrMaster = "New From Master";
+    }
+}
